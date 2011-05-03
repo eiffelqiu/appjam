9b6ec0373c6bf0c40ebedd203ab46971ca379181
Eiffel Q <eiffelqiu@gmail.com>
2011-05-03 09:40:18 +0800

    0.1.3

--- lib/appjam/version.rb
+++ lib/appjam/version.rb
@@ -2,7 +2,7 @@ module Appjam
   module Version
     MAJOR = 0
     MINOR = 1
-    PATCH = 2
+    PATCH = 3
     BUILD = ''
     
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.').chomp('.')
