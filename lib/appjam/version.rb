5aa62457687a414dd3057add0b706062eed3da30
Eiffel Q <eiffelqiu@gmail.com>
2011-05-26 11:33:31 +0800

    fix sh error

--- lib/appjam/version.rb
+++ lib/appjam/version.rb
@@ -3,7 +3,7 @@ module Appjam
     MAJOR = 0
     MINOR = 1
     PATCH = 8
-    BUILD = 'pre10'
+    BUILD = 'pre11'
     
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.').chomp('.')
   end
