f40a9bade66cbfd06b0557bb5f0aab0fb55a7156
Eiffel Q <eiffelqiu@gmail.com>
2011-05-27 11:20:46 +0800

    remote fetch release

--- lib/appjam/version.rb
+++ lib/appjam/version.rb
@@ -3,7 +3,7 @@ module Appjam
     MAJOR = 0
     MINOR = 1
     PATCH = 8
-    BUILD = 'pre12'
+    BUILD = 'pre13'
     
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.').chomp('.')
   end
